77c264869dc43653fc75529aa70802f0d6b89a41
Leonte Ionut-Claudiu <leonteionut98@yahoo.com>
2024-01-14 19:08:49 +0200

    banner

---
+++ resources/views/site/blocks/banner.blade.php
@@ -0,0 +1,6 @@
+<div
+    class="flex flex-col justify-between w-full mb-6 transition duration-500 ease-in-out bg-white rounded-md shadow-lg hover:shadow-2xl">
+    <div>
+        <img src="{{ $block->image('3/1', '3/1') }}" alt="{{ env('APP_NAME') }}" class="object-cover " />
+    </div>
+</div>
---
+++ resources/views/twill/blocks/banner.blade.php
@@ -0,0 +1,5 @@
+@twillBlockTitle('Banner')
+@twillBlockIcon('image')
+@twillBlockGroup('app')
+
+<x-twill::medias name="3/1" label="Image" />
