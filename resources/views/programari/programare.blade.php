67dc5452cfaa8b9239b55ea5961abad3c5de31a9
Leonte Ionut-Claudiu <leonteionut98@yahoo.com>
2024-04-14 17:52:48 +0300

    commit

--- resources/views/programari/programare.blade.php
+++ resources/views/programari/programare.blade.php
@@ -6,61 +6,80 @@
                 {{ session('success') }}
             </div>
         @endif
-
-        <div class="flex gap-10 flex-col items-center lg:flex-row w-fit mx-auto">
-
-            <div class="w-full text-justify max-w-[75ch] mx-auto flex items-center flex-col gap-4">
-                <h5>Completați formularul nostru de mai jos
-                    pentru a asigura îngrijirea optimă a vehiculului dvs. Echipa noastră calificată este pregătită să vă
-                    ofere servicii de înaltă calitate.</h5>
-                <img src="{{ asset('images/programare.jpg') }}" alt="Logo" class="rounded-md max-h-[768px]" />
+        <h5 class="text-center text-economic-darkgreen max-w-[75ch] w-fit mx-auto mb-4">Completati formularul de mai jos
+            pentru a solicita o programare in service-ul nostru si va vom contacta in cel mai scurt timp posibil. Echipa noastra este pregatita sa va
+            ofere servicii de inalta calitate.</h5>
+            <p class="text-center text-gray-700 mb-2">Sau ne puteti contacta :</p>
+            <div class="w-fit mx-auto mb-6 flex flex-wrap items-center justify-between gap-3 md:gap-5">
+                {{-- <img src="{{ asset('images/programare.jpg') }}" alt="Logo" class="rounded-md max-h-[768px]" /> --}}
+                <a href="tel:+40744322011"
+                    class='flex items-center gap-1 mx-auto transition w-fit hover:underline text-lg md:text-xl text-economic-darkgreen'>
+                    <i class="fa-solid fa-phone"></i> 0744 322 011
+                </a>
+                <a href="tel:+40746050608"
+                    class='flex items-center gap-1 mx-auto transition w-fit hover:underline text-lg md:text-xl text-economic-darkgreen'>
+                    <i class="fa-solid fa-phone"></i> 0746 050 608
+                </a>
+                <a href="tel:+40742223007"
+                    class='flex items-center gap-1 mx-auto transition w-fit hover:underline text-lg md:text-xl text-economic-darkgreen'>
+                    <i class="fa-solid fa-phone"></i> 0742 223 007
+                </a>
+                <a href="mailto:office@serviceeconomic.ro"
+                    class='flex items-center gap-1 mx-auto transition w-fit hover:underline text-lg md:text-xl text-economic-darkgreen'>
+                    office@serviceeconomic.ro
+                    <i class="fa fa-envelope text-economic-darkgreen"></i>
+                </a>
             </div>
+        <div class="flex gap-10 flex-col items-start lg:flex-row max-w-5xl mx-auto">
 
             <form action="/programare" method="post"
-                class="max-w-lg mx-auto bg-white p-2 md:p-8 rounded shadow-md hover:shadow-lg transition hover:shadow-economic-darkgreen shadow-economic-darkgreen flex-grow w-full programare-form">
+                class="mx-auto bg-white p-2 md:p-8 rounded shadow-md hover:shadow-lg transition hover:shadow-economic-darkgreen shadow-economic-darkgreen flex-grow w-full programare-form">
                 @csrf
 
-                <h4 class="text-center mb-4">Formular programare service</h4>
+                <h4 class="text-center mb-4">Formular programare service <i
+                        class="fa-solid fa-screwdriver-wrench text-economic-darkgreen"></i></h4>
 
-                <div class="mb-4">
-                    <label for="nume" class="block text-gray-600 font-bold">Nume:</label>
-                    <input type="text" name="nume" id="nume" data-programare-form-name='nume'
-                        class="@error('nume') bg-red-50 @enderror ring-economic-darkgreen focus:ring-economic-darkgreen"
-                        placeholder="Popescu" value="{{ old('nume') }}">
-                    @error('nume')
-                        <span class="text-red-500">{{ $message }}</span>
-                    @enderror
+                <div class="mb-4 flex justify-between items-center gap-3">
+                    <div class="flex-grow">
+                        <label for="nume" class="block text-gray-600 font-bold">Nume:</label>
+                        <input type="text" name="nume" id="nume" data-programare-form-name='nume'
+                            class="w-full @error('nume') bg-red-50 @enderror ring-economic-darkgreen focus:ring-economic-darkgreen"
+                            placeholder="Alexandru Popescu" value="{{ old('nume') }}">
+                        @error('nume')
+                            <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="flex-grow">
+                        <label for="email" class="block text-gray-600 font-bold">Email:</label>
+                        <input type="email" name="email" id="email" data-programare-form-name='email'
+                            class="w-full @error('email') bg-red-50 @enderror focus:ring-economic-darkgreen ring-economic-darkgreen"
+                            placeholder="email@example.com" value="{{ old('email') }}">
+                        @error('email')
+                            <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
                 </div>
 
-                <div class="mb-4">
-                    <label for="email" class="block text-gray-600 font-bold">Email:</label>
-                    <input type="email" name="email" id="email" data-programare-form-name='email'
-                        class="@error('email') bg-red-50 @enderror focus:ring-economic-darkgreen ring-economic-darkgreen"
-                        placeholder="Alexandru" value="{{ old('email') }}">
-                    @error('email')
-                        <span class="text-red-500">{{ $message }}</span>
-                    @enderror
-                </div>
-
-                <div class="mb-4">
-                    <label for="nr" class="block text-gray-600 font-bold">Telefon:</label>
-                    <input type="text" name="nr" id="nr" data-programare-form-name='nr'
-                        class="@error('nr') bg-red-50 @enderror focus:ring-economic-darkgreen ring-economic-darkgreen"
-                        placeholder="0712345678" value="{{ old('nr') }}">
-                    @error('nr')
-                        <span class="text-red-500">{{ $message }}</span>
-                    @enderror
-                </div>
-
-                <div class="mb-4">
-                    <label for="nr_inmatriculare" class="block text-gray-600 font-bold">Nr. Înmatriculare:</label>
-                    <input type="text" name="nr_inmatriculare" id="nr_inmatriculare"
-                        data-programare-form-name='nr_inmatriculare'
-                        class="@error('nr_inmatriculare') bg-red-50 @enderror focus:ring-economic-darkgreen ring-economic-darkgreen"placeholder="B183ARJ"
-                        value="{{ old('nr_inmatriculare') }}">
-                    @error('nr_inmatriculare')
-                        <span class="text-red-500">{{ $message }}</span>
-                    @enderror
+                <div class="mb-4 flex justify-between items-center gap-3">
+                    <div class="flex-grow">
+                        <label for="nr" class="block text-gray-600 font-bold">Telefon:</label>
+                        <input type="text" name="nr" id="nr" data-programare-form-name='nr'
+                            class="w-full @error('nr') bg-red-50 @enderror focus:ring-economic-darkgreen ring-economic-darkgreen"
+                            placeholder="0712345678" value="{{ old('nr') }}">
+                        @error('nr')
+                            <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="flex-grow">
+                        <label for="nr_inmatriculare" class="block text-gray-600 font-bold">Nr. Înmatriculare:</label>
+                        <input type="text" name="nr_inmatriculare" id="nr_inmatriculare"
+                            data-programare-form-name='nr_inmatriculare'
+                            class="w-full @error('nr_inmatriculare') bg-red-50 @enderror focus:ring-economic-darkgreen ring-economic-darkgreen"placeholder="B183ARJ"
+                            value="{{ old('nr_inmatriculare') }}">
+                        @error('nr_inmatriculare')
+                            <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
                 </div>
 
                 <div class="mb-4">
@@ -123,7 +142,9 @@
             // If the selected date is Sunday, show the error message
             if (isSunday(selectedDate)) {
                 dateError.classList.remove('hidden');
-                event.target.setCustomValidity('Programul nostru este: luni-vineri orele 08:00 - 17:00 si sambata orele 08:00 - 14:00.');
+                event.target.setCustomValidity(
+                    'Programul nostru este: luni-vineri orele 08:00 - 17:00 si sambata orele 08:00 - 14:00.'
+                );
             } else {
                 dateError.classList.add('hidden');
                 event.target.setCustomValidity('');
